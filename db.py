914f95550c784144f9137aa230d1fb592d1cabe3
antoniopalmeirim <a.palmeirim03@gmail.com>
2025-10-03 11:52:06 +0100

    Simple CLI + SQLite3 database, first implementation

---
+++ db.py
@@ -0,0 +1,35 @@
+import sqlite3
+
+DBNAME = "books.db"
+
+def create_table():
+    execute("""
+                   CREATE TABLE IF NOT EXISTS books(
+                   id INTEGER PRIMARY KEY AUTOINCREMENT,
+                   title TEXT NOT NULL,
+                   author TEXT,
+                   year INTEGER,
+                   genre TEXT,
+                   rating INTEGER,
+                   notes TEXT)
+        """)
+
+def add_book(title, author, year, genre, rating, notes):
+    execute("""
+                    INSERT INTO books (title, author, year, genre, rating, notes)
+                    VALUES (?,?,?,?,?,?)""", (title, author, year, genre, rating, notes))
+    
+def delete_book(book_id):
+    execute("DELETE FROM books WHERE id = ?", (book_id,))
+
+def list_books():
+    return execute("SELECT * FROM books", fetch=True)
+
+
+def execute(query, params = (), fetch=False):
+    with sqlite3.connect(DBNAME) as conn:
+        cursor = conn.cursor()
+        cursor.execute(query, params)
+        if fetch:
+            return cursor.fetchall()
+        conn.commit()
